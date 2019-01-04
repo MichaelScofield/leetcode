9d79d451640a938563f1f6470e0bafc6fcce6967
Lucas Guigui <bayinamine@gmail.com>
2019-01-04 10:55:31 +0800

    https://leetcode.com/problems/two-sum-iv-input-is-a-bst/

---
+++ p653/solution.go
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findTarget(root *TreeNode, k int) bool {
+    return traverse(root, root, k)
+}
+
+func traverse(root *TreeNode, curNode *TreeNode, k int) bool {
+    remaining := k - curNode.Val
+    if findNum(root, curNode.Val, remaining) {
+        return true
+    } else {
+        if curNode.Left != nil {
+            if traverse(root, curNode.Left, k) {
+                return true
+            }
+        }
+        if curNode.Right != nil {
+            if traverse(root, curNode.Right, k) {
+                return true
+            }
+        }
+    }
+    return false;
+}
+
+func findNum(root *TreeNode, except int, num int) bool {
+    if root.Val == num && root.Val != except {
+        return true
+    } else if num < root.Val {
+        if root.Left != nil {
+            return findNum(root.Left, except, num)
+        }
+    } else {
+        if root.Right != nil {
+            return findNum(root.Right, except, num)
+        }
+    }
+    return false
+}
