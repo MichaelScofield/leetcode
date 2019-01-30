7708fdce41dd2e477c5a53a51fb4b5802b1ae71d
Lucas Guigui <bayinamine@gmail.com>
2019-01-30 10:21:17 +0800

    https://leetcode.com/problems/fibonacci-number/

---
+++ p509. Fibonacci Number/solution.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn fib(n: i32) -> i32 {
+        match n {
+            0 => 0,
+            1 => 1,
+            _ => Self::fib(n - 1) + Self::fib(n - 2)
+        }
+    }
+}
