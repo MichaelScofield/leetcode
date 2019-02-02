dd32f60fbad78ae93fb11c144045ab5d0980c8e3
Lucas Guigui <bayinamine@gmail.com>
2019-02-02 18:48:25 +0800

    https://leetcode.com/problems/distribute-candies/

--- p575. Distribute Candies/solution.rs
+++ p575. Distribute Candies/solution.rs
@@ -1,9 +1,10 @@
 impl Solution {
     pub fn distribute_candies(candies: Vec<i32>) -> i32 {
-        let sister_candies = (candies.len() / 2) as i32;
+        let total_candies = candies.len();
+        let sister_candies = (total_candies / 2) as i32;
         let mut kind = 0;
         use std::collections::HashSet;
-        let mut candies_kinds = HashSet::new();
+        let mut candies_kinds = HashSet::with_capacity(total_candies);
         for candy in candies.into_iter() {
             if candies_kinds.insert(candy) {
                 kind += 1;
