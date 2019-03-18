67079d3dbcb855fe0efa67293771fe16a07b382a
Lucas Guigui <bayinamine@gmail.com>
2019-03-18 14:32:08 +0800

    https://leetcode.com/problems/repeated-string-match/

--- p0686. Repeated String Match/solution.rs
+++ p0686. Repeated String Match/solution.rs
@@ -1,21 +1,19 @@
 impl Solution {
     pub fn repeated_string_match(a: String, b: String) -> i32 {
-        let chars_a: Vec<char> = a.chars().collect();
-        let chars_b: Vec<char> = b.chars().collect();
-        let len_a = chars_a.len();
-        let len_b = chars_b.len();
-        let mut p;
-        'l: for i in 0..len_a {
-            p = i;
-            for j in 0..len_b {
-                if chars_b[j] == chars_a[p % len_a] {
-                    p += 1;
-                } else {
-                    continue 'l;
-                }
-            }
-            return (p / len_a + if p % len_a > 0 { 1 } else { 0 }) as i32;
+        let mut s = String::new();
+        let a = a.as_str();
+        let b = b.as_str();
+        let l = b.len();
+        let mut repeated = 0;
+        while s.len() < l {
+            s.push_str(a);
+            repeated += 1;
         }
-        -1
+        if s.contains(b) {
+            repeated
+        } else {
+            s.push_str(a);
+            if s.contains(b) { repeated + 1 } else { -1 }
+        }
     }
 }
