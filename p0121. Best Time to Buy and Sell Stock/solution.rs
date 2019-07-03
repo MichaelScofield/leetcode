4a6e4e340c0c3e736361d9998ef0627ce1ea4fa7
Lucas Guigui <bayinamine@gmail.com>
2019-07-03 17:28:35 +0800

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

--- p0121. Best Time to Buy and Sell Stock/solution.rs
+++ p0121. Best Time to Buy and Sell Stock/solution.rs
@@ -1,10 +1,16 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
+        if prices.len() < 1 {
+            return 0;
+        }
+        let mut min_price = prices[0];
         let mut max_profit = 0;
-        let l = prices.len();
-        for (i, price) in prices.iter().enumerate() {
-            for j in i + 1..l {
-                let profit = prices[j] - *price;
+        for i in 1..prices.len() {
+            let price = prices[i];
+            if price < min_price {
+                min_price = price;
+            } else {
+                let profit = price - min_price;
                 if profit > max_profit {
                     max_profit = profit;
                 }
