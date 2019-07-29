bd0a498d19479ae5b220911de57c5b610e815411
Lucas Guigui <bayinamine@gmail.com>
2019-07-29 10:26:11 +0800

    https://leetcode.com/problems/pascals-triangle/

--- p0118. Pascal's Triangle/solution.rs
+++ p0118. Pascal's Triangle/solution.rs
@@ -1,17 +1,23 @@
 impl Solution {
     pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
         let num_rows = num_rows as usize;
-        let mut pascal_triangle: Vec<Vec<i32>> = Vec::with_capacity(num_rows);
+        let mut pascal_triangle = Vec::with_capacity(num_rows);
         for i in 0..num_rows {
-            let mut current_row = Vec::with_capacity(i + 1);
-            if i > 0 {
+            if i == 0 {
+                pascal_triangle.push(vec![1]);
+            } else {
                 let last_row = &pascal_triangle[i - 1];
+                let mut new_row = Vec::with_capacity(last_row.len() + 1);
                 for j in 0..last_row.len() {
-                    current_row.push(last_row[j] + if j > 0 { last_row[j - 1] } else { 0 });
+                    if j == 0 {
+                        new_row.push(1);
+                    } else {
+                        new_row.push(last_row[j - 1] + last_row[j]);
+                    }
                 }
+                new_row.push(1);
+                pascal_triangle.push(new_row);
             }
-            current_row.push(1);
-            pascal_triangle.push(current_row);
         }
         pascal_triangle
     }
