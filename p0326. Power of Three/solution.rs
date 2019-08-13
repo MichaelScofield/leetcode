d4226e1528203e02d79ee604dca4053848e3d44b
Lucas Guigui <bayinamine@gmail.com>
2019-08-13 02:26:02 +0000

    https://leetcode.com/problems/power-of-three/

---
+++ p0326. Power of Three/solution.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn is_power_of_three(n: i32) -> bool {
+        if n <= 0 {
+            return false;
+        }
+        let mut n = n;
+        while n > 1 {
+            if n % 3 != 0 {
+                return false;
+            }
+            n /= 3;
+        }
+        true
+    }
+}
