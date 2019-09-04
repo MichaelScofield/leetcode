535c60b8656f7726e276737ff2b48ad3d703d23d
Lucas Guigui <bayinamine@gmail.com>
2019-09-04 11:17:24 +0800

    https://leetcode.com/problems/path-sum/

---
+++ p0112. Path Sum/solution.rs
@@ -0,0 +1,35 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn has_path_sum(root: Option<Rc<RefCell<TreeNode>>>, sum: i32) -> bool {
+        if let Some(node) = root {
+            let node = node.borrow();
+            if node.left.is_none() && node.right.is_none() {
+                node.val == sum
+            } else {
+                Solution::has_path_sum(node.left.clone(), sum - node.val) ||
+                    Solution::has_path_sum(node.right.clone(), sum - node.val)
+            }
+        } else {
+            false
+        }
+    }
+}
