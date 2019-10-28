68c2dc403a55d1f8d8a9e13b5fc166b98b3d9a1e
Lucas Guigui <bayinamine@gmail.com>
2019-10-28 14:33:16 +0800

    https://leetcode.com/problems/split-a-string-in-balanced-strings/

---
+++ p1221. Split a String in Balanced Strings/solution.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn balanced_string_split(s: String) -> i32 {
+        let mut stack = 0;
+        let mut count = 0;
+        for c in s.chars() {
+            match c {
+                'R' => stack += 1,
+                'L' => stack -= 1,
+                _ => panic!()
+            }
+            if stack == 0 {
+                count += 1;
+            }
+        }
+        count
+    }
+}
