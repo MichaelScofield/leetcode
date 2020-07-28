36c07e31feeb8a675a2a3049a4133df854c39b83
Lucas Guigui <bayinamine@gmail.com>
2020-07-28 10:57:50 +0800

    https://leetcode.com/problems/permutations/

---
+++ p0046. Permutations/solution.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        if nums.len() < 1 {
+            return Vec::with_capacity(0);
+        }
+        fn permute(nums: Vec<i32>, i: usize, rs: &mut Vec<Vec<i32>>) {
+            if i == nums.len() {
+                rs.push(nums);
+                return;
+            }
+            for j in i..nums.len() {
+                let mut nums = nums.clone();
+                nums.swap(i, j);
+                permute(nums, i + 1, rs);
+            }
+        }
+        let mut rs = vec![];
+        permute(nums, 0, &mut rs);
+        rs
+    }
+}
