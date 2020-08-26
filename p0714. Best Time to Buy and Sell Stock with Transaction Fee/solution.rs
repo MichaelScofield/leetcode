d37e3bc25a42c3b7ca96d66c36a5b9c66168f49e
Lucas Guigui <bayinamine@gmail.com>
2020-08-26 13:55:05 +0800

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/

---
+++ p0714. Best Time to Buy and Sell Stock with Transaction Fee/solution.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>, fee: i32) -> i32 {
+        if prices.len() == 0 {
+            return 0;
+        }
+        let n = prices.len();
+        let mut dp = vec![vec![0, 0]; n + 1];
+        dp[0][0] = 0;
+        dp[0][1] = std::i32::MIN;
+        for x in 1..n + 1 {
+            dp[x][0] = std::cmp::max(dp[x - 1][0], dp[x - 1][1] + prices[x - 1]);
+            dp[x][1] = std::cmp::max(dp[x - 1][0] - prices[x - 1] - fee, dp[x - 1][1]);
+        }
+        std::cmp::max(0, dp[n][0])
+    }
+}
