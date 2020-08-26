81e7a2a2ff9e1f54e70226207b7f17bc1eeeebe7
Lucas Guigui <bayinamine@gmail.com>
2020-08-26 12:35:00 +0800

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/

---
+++ p0123. Best Time to Buy and Sell Stock III/solution.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        if prices.len() == 0 {
+            return 0;
+        }
+        let n = prices.len();
+        let k = 2;
+        let mut dp = vec![vec![vec![0, 0]; k + 1]; n + 1];
+        for x in 0..n + 1 {
+            dp[x][0][0] = 0;
+            dp[x][0][1] = std::i32::MIN;
+        }
+        for x in 0..k + 1 {
+            dp[0][x][0] = 0;
+            dp[0][x][1] = std::i32::MIN;
+        }
+        for x in 1..n + 1 {
+            for y in 1..k + 1 {
+                dp[x][y][0] = std::cmp::max(dp[x - 1][y][0], dp[x - 1][y][1] + prices[x - 1]);
+                dp[x][y][1] = std::cmp::max(dp[x - 1][y - 1][0] - prices[x - 1], dp[x - 1][y][1]);
+            }
+        }
+        dp[n][k][0]
+    }
+}
