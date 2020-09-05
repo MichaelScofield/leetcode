bd31f5799ef287144c8eea49540d198504bfe77a
Lucas Guigui <bayinamine@gmail.com>
2020-09-05 11:36:17 +0800

    https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/

---
+++ 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面/solution.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn exchange(nums: Vec<i32>) -> Vec<i32> {
+        if nums.len() == 0 {
+            return nums;
+        }
+        let mut i = 0;
+        let mut j = nums.len() - 1;
+        let nums = &mut { nums };
+        while i < j {
+            while i < j {
+                if nums[i] % 2 == 0 {
+                    break;
+                }
+                i += 1;
+            }
+            while i < j {
+                if nums[j] % 2 == 1 {
+                    break;
+                }
+                j -= 1;
+            }
+            nums.swap(i, j);
+        }
+        nums.to_owned()
+    }
+}
