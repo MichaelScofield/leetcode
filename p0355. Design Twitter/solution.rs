1462768d13b88239fb5ffa9d2c7e38e6b2ab8923
Li Desheng <bayinamine@gmail.com>
2020-09-15 12:15:22 +0800

    https://leetcode.com/problems/design-twitter/

---
+++ p0355. Design Twitter/solution.rs
@@ -0,0 +1,107 @@
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet};
+use std::time::Instant;
+
+struct Twitter {
+    relation: HashMap<i32, HashSet<i32>>,
+    tweets: HashMap<i32, Vec<Tweet>>,
+}
+
+#[derive(Eq, PartialEq)]
+struct Tweet {
+    tweet_id: i32,
+    tweet_time: Instant,
+}
+
+impl PartialOrd for Tweet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let order = other.tweet_time.partial_cmp(&self.tweet_time);
+        if let Some(Ordering::Equal) = order {
+            other.tweet_id.partial_cmp(&self.tweet_id)
+        } else {
+            order
+        }
+    }
+}
+
+impl Ord for Tweet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let order = other.tweet_time.cmp(&self.tweet_time);
+        if Ordering::Equal == order {
+            other.tweet_id.cmp(&self.tweet_id)
+        } else {
+            order
+        }
+    }
+}
+
+struct Feed<'a> {
+    recent_tweets: Vec<&'a Tweet>,
+}
+
+impl<'a> Feed<'a> {
+    fn new() -> Self {
+        Feed { recent_tweets: Vec::with_capacity(10) }
+    }
+
+    fn insert_tweets(&mut self, tweets: &'a Vec<Tweet>) {
+        for tweet in tweets.iter() {
+            if self.recent_tweets.len() == 10 {
+                if self.recent_tweets.last().expect("illegal state").lt(&tweet) {
+                    continue;
+                }
+                self.recent_tweets.pop();
+            }
+            self.recent_tweets.push(tweet);
+            self.recent_tweets.sort();
+        }
+    }
+}
+
+impl Twitter {
+    fn new() -> Self {
+        Twitter {
+            relation: HashMap::new(),
+            tweets: HashMap::new(),
+        }
+    }
+
+    fn post_tweet(&mut self, user_id: i32, tweet_id: i32) {
+        let tweet = Tweet { tweet_id, tweet_time: Instant::now() };
+        self.tweets.entry(user_id).or_insert_with(|| Vec::new()).push(tweet);
+    }
+
+    fn get_news_feed(&self, user_id: i32) -> Vec<i32> {
+        let mut feed = Feed::new();
+        if let Some(tweets) = self.tweets.get(&user_id) {
+            feed.insert_tweets(tweets);
+        }
+
+        if let Some(followers) = self.relation.get(&user_id) {
+            for follower in followers.iter() {
+                let tweets = self.tweets.get(follower);
+                if let Some(tweets) = tweets {
+                    feed.insert_tweets(tweets)
+                }
+            }
+        }
+        feed.recent_tweets.iter().map(|&tweet| tweet.tweet_id).collect()
+    }
+
+    fn follow(&mut self, follower_id: i32, followee_id: i32) {
+        if follower_id == followee_id {
+            return;
+        }
+        self.relation.entry(follower_id).or_insert_with(|| HashSet::new()).insert(followee_id);
+    }
+
+    fn unfollow(&mut self, follower_id: i32, followee_id: i32) {
+        if follower_id == followee_id {
+            return;
+        }
+        let relation = self.relation.get_mut(&follower_id);
+        if let Some(relation) = relation {
+            relation.remove(&followee_id);
+        }
+    }
+}
