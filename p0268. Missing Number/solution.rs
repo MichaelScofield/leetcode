b27736b51340ad924d598e59b588802bb793471f
Li Desheng <bayinamine@gmail.com>
2020-09-17 23:06:56 +0800

    https://leetcode.com/problems/missing-number/

--- p0268. Missing Number/solution.rs
+++ p0268. Missing Number/solution.rs
@@ -1,7 +1,17 @@
 impl Solution {
     pub fn missing_number(nums: Vec<i32>) -> i32 {
-        let sum: i32 = nums.iter().sum();
-        let total = (nums.len() * (nums.len() + 1) / 2) as i32;
-        total - sum
+        let nums = &mut { nums };
+        nums.sort();
+        let mut i = 0;
+        let mut j = nums.len() as i32 - 1;
+        while i <= j {
+            let mid = (j - i) / 2 + i;
+            if nums[mid as usize] == mid {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        i
     }
 }
