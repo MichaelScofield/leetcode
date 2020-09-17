b7bff7a6c6957aa805db99a77f44c9862bf6a4b4
Li Desheng <bayinamine@gmail.com>
2020-09-17 23:00:57 +0800

    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/

--- p0034. Find First and Last Position of Element in Sorted Array/solution.rs
+++ p0034. Find First and Last Position of Element in Sorted Array/solution.rs
@@ -1,39 +1,32 @@
 impl Solution {
     pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let len = nums.len();
-        if len == 0 {
+        if nums.len() == 0 {
             return vec![-1, -1];
         }
-        let mut i = 0;
-        let mut j = len;
-        let mut range = vec![-1, -1];
-        while i < j {
-            let mid = i + (j - i) / 2;
-            if nums[mid] < target {
-                i = mid + 1;
-            } else if nums[mid] > target {
-                j = mid;
-            } else {
-                let mut p = mid;
-                while p > 0 {
-                    if nums[p - 1] != target {
-                        break;
+        fn bound_binary_search(nums: &Vec<i32>, target: i32, left: bool) -> i32 {
+            let mut i = 0 as i32;
+            let mut j = (nums.len() - 1) as i32;
+            while i <= j {
+                let mid = (j - i) / 2 + i;
+                if nums[mid as usize] == target {
+                    if left {
+                        j = mid - 1;
+                    } else {
+                        i = mid + 1;
                     }
-                    p -= 1;
+                } else if nums[mid as usize] > target {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
                 }
-                range[0] = p as i32;
-
-                let mut q = mid;
-                while q < len - 1 {
-                    if nums[q + 1] != target {
-                        break;
-                    }
-                    q += 1;
-                }
-                range[1] = q as i32;
-                break;
             }
+            if left { j + 1 } else { i - 1 }
         }
-        range
+        let i = bound_binary_search(&nums, target, true);
+        if i == -1 || i == nums.len() as i32 || nums[i as usize] != target {
+            return vec![-1, -1];
+        }
+        let j = bound_binary_search(&nums, target, false);
+        vec![i, j]
     }
 }
