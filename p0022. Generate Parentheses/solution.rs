2bd1f89a7a56c8bb7c0d1d0ac02a9e0bd2701000
Li Desheng <bayinamine@gmail.com>
2020-09-18 11:09:51 +0800

    https://leetcode.com/problems/generate-parentheses/

--- p0022. Generate Parentheses/solution.rs
+++ p0022. Generate Parentheses/solution.rs
@@ -3,38 +3,43 @@ impl Solution {
         if n < 1 {
             return Vec::with_capacity(0);
         }
-        use std::collections::HashSet;
-        let mut parenthesis_set = HashSet::new();
-        parenthesis_set.insert(vec![b'(', b')']);
-        for _i in 2..=n {
-            let mut new_set = HashSet::new();
-            for parenthesis in parenthesis_set {
-                let l = parenthesis.len();
-                let mut r1 = vec![b'0'; l + 2];
-                r1[1..=l].copy_from_slice(&parenthesis);
-                r1[0] = b'(';
-                r1[l + 1] = b')';
-                new_set.insert(r1);
 
-                let mut r2 = vec![b'0'; l + 2];
-                r2[..l].copy_from_slice(&parenthesis);
-                r2[l] = b'(';
-                r2[l + 1] = b')';
-                new_set.insert(r2);
-
-                for (i, &x) in parenthesis.iter().enumerate() {
-                    if x == b'(' {
-                        let mut r = parenthesis.clone();
-                        r.insert(i + 1, b'(');
-                        r.insert(i + 2, b')');
-                        new_set.insert(r);
+        fn is_valid(parenthesis: &Vec<char>) -> bool {
+            let len = parenthesis.len();
+            let mut tmp = Vec::with_capacity(len);
+            for &x in parenthesis {
+                if x == ')' {
+                    if let Some(&top) = tmp.last() {
+                        if top == '(' {
+                            tmp.pop();
+                            continue;
+                        }
                     }
                 }
+                tmp.push(x);
             }
-            parenthesis_set = new_set;
+            tmp.is_empty()
         }
-        parenthesis_set.into_iter()
-            .map(|parenthesis| String::from_utf8(parenthesis).unwrap())
-            .collect()
+
+        fn backtrace(solution: &mut Vec<String>, len: usize, parenthesis: &mut Vec<char>) {
+            if parenthesis.len() == len {
+                if is_valid(parenthesis) {
+                    solution.push(parenthesis.clone().into_iter().collect());
+                }
+                return;
+            }
+
+            for &c in ['(', ')'].iter() {
+                parenthesis.push(c);
+                backtrace(solution, len, parenthesis);
+                parenthesis.pop();
+            }
+        }
+
+        let mut solution = vec![];
+        let len = n as usize * 2;
+        let mut parenthesis = vec![];
+        backtrace(&mut solution, len, &mut parenthesis);
+        solution
     }
 }
